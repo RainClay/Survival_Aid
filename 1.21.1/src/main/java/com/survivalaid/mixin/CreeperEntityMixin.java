58ca1cb949378a0423f5181fa1d0f4b299d8af77
Your Name <you@example.com>
2026-08-02 23:07:26 +0800

    Fix CreeperEntityMixin createExplosion target across versions
    The @Redirect targeted a nonexistent createExplosion overload that
    included an extra boolean (Z) param. CreeperEntity.explode actually
    calls createExplosion(Entity, double, double, double, float,
    ExplosionSourceType) with no boolean. This caused "Scanned 0 target(s)"
    at runtime.
    
    Switch to @ModifyArg(index=5) on the ExplosionSourceType arg for
    robustness (matching the 26.1.2 design):
    - 1.21, 1.21.1: World.createExplosion(...)Explosion
    - 1.21.2+: ServerWorld.createExplosion(...)V

--- 1.21.1/src/main/java/com/survivalaid/mixin/CreeperEntityMixin.java
+++ 1.21.1/src/main/java/com/survivalaid/mixin/CreeperEntityMixin.java
@@ -1,22 +1,16 @@
 package com.survivalaid.mixin;
 
 import com.survivalaid.features.CreeperGriefingControlRule;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.mob.CreeperEntity;
-import net.minecraft.world.World;
 import net.minecraft.world.World.ExplosionSourceType;
-import net.minecraft.world.explosion.Explosion;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(CreeperEntity.class)
 public abstract class CreeperEntityMixin {
-    @Redirect(method = "explode", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;createExplosion(Lnet/minecraft/entity/Entity;DDDFZLnet/minecraft/world/World$ExplosionSourceType;)Lnet/minecraft/world/explosion/Explosion;"))
-    private Explosion survivalAid$controlCreeperBlockDamage(World world, Entity entity, double x, double y, double z, float power, boolean causeFire, ExplosionSourceType sourceType) {
-        if (CreeperGriefingControlRule.survivalAidCreeperGriefingControl) {
-            return world.createExplosion(entity, x, y, z, power, causeFire, ExplosionSourceType.NONE);
-        }
-        return world.createExplosion(entity, x, y, z, power, causeFire, sourceType);
+    @ModifyArg(method = "explode", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;createExplosion(Lnet/minecraft/entity/Entity;DDDFLnet/minecraft/world/World$ExplosionSourceType;)Lnet/minecraft/world/explosion/Explosion;"), index = 5)
+    private ExplosionSourceType survivalAid$controlCreeperBlockDamage(ExplosionSourceType sourceType) {
+        return CreeperGriefingControlRule.survivalAidCreeperGriefingControl ? ExplosionSourceType.NONE : sourceType;
     }
 }
--- 1.21.11/src/main/java/com/survivalaid/mixin/CreeperEntityMixin.java
+++ 1.21.11/src/main/java/com/survivalaid/mixin/CreeperEntityMixin.java
@@ -1,18 +1,16 @@
 package com.survivalaid.mixin;
 
 import com.survivalaid.features.CreeperGriefingControlRule;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.mob.CreeperEntity;
-import net.minecraft.world.World;
 import net.minecraft.world.World.ExplosionSourceType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(CreeperEntity.class)
 public abstract class CreeperEntityMixin {
-    @Redirect(method = "explode", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;createExplosion(Lnet/minecraft/entity/Entity;DDDFZLnet/minecraft/world/World$ExplosionSourceType;)V"))
-    private void survivalAid$controlCreeperBlockDamage(World world, Entity entity, double x, double y, double z, float power, boolean causeFire, ExplosionSourceType sourceType) {
-        world.createExplosion(entity, x, y, z, power, causeFire, CreeperGriefingControlRule.survivalAidCreeperGriefingControl ? ExplosionSourceType.NONE : sourceType);
+    @ModifyArg(method = "explode", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerWorld;createExplosion(Lnet/minecraft/entity/Entity;DDDFLnet/minecraft/world/World$ExplosionSourceType;)V"), index = 5)
+    private ExplosionSourceType survivalAid$controlCreeperBlockDamage(ExplosionSourceType sourceType) {
+        return CreeperGriefingControlRule.survivalAidCreeperGriefingControl ? ExplosionSourceType.NONE : sourceType;
     }
 }
